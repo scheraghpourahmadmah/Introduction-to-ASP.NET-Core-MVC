92bde0f1c927b3188f8fea80cf3ad39a36807d97
scheraghpourahmadmah <scheraghpourahmadmah@myseneca.ca>
2022-11-02 19:52:21 -0400

    intro-project

---
+++ WebApplication2/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication2/Models/Sholeh.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication2.Models
+{
+    public class Sholeh
+    {
+        public int OrderNumber { get; set; }
+        public string Name { get; set; }
+    }
+}
